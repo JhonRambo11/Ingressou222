7cc38c480c0e2c996813392978ceaf50597bcd3e
JhonRambo11 <marcelo_p_alexandre@estudante.sesisenai.org.br>
2024-10-08 20:24:11 -0300

    aaaa

---
+++ Ingressou/src/main/java/com/ingressou/Ingressou/IngressouApplication.java
@@ -0,0 +1,13 @@
+package com.ingressou.Ingressou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressouApplication.class, args);
+	}
+
+}
